3775699178d524d544b5fee8ff23f3e4a56eb573
BuildTools <unconfigured@null.spigotmc.org>
2023-12-16 00:40:47 +0100

    Docs hinzugefügt

--- src/main/java/de/tubyoub/seatReservationSystem/model/Train.java
+++ src/main/java/de/tubyoub/seatReservationSystem/model/Train.java
@@ -1,16 +1,38 @@
 package de.tubyoub.seatReservationSystem.model;
 import de.tubyoub.seatReservationSystem.util.List;
 
+/**
+ * This class represents a train, which consists of a list of wagons.
+ */
 public class Train {
+  // List of wagons in the train
   List<Wagon> wagonList;
+  // Identifier for the train
   String id;
-  public Train(String pBezeichner) {
+
+  /**
+   * Constructor for the Train class.
+   * Initializes the list of wagons and the identifier of the train.
+   * @param pTrainName The identifier of the train
+   */
+  public Train(String pTrainName) {
     wagonList = new List<Wagon>();
-    id = pBezeichner;
+    id = pTrainName;
   }
+
+  /**
+   * Appends a new wagon to the train.
+   * @param pWagonID The identifier of the new wagon
+   * @param pSeatQuantity The number of seats in the new wagon
+   */
   public void appendNewWagon(int pWagonID, int pSeatQuantity){
     this.wagonList.append(new Wagon(pWagonID, pSeatQuantity));
   }
+
+  /**
+   * Clears all reservations in a specific wagon.
+   * @param pWagonID The identifier of the wagon to clear
+   */
   public void clearWagon(int pWagonID){
     searchForWagon(pWagonID);
     Wagon wagonToClear = wagonList.getContent();
@@ -21,20 +43,54 @@ public class Train {
       }
     }
   }
+
+  /**
+   * Returns the identifier of the train.
+   * @return The identifier of the train
+   */
   public String getIdentifier() {
     return id;
   }
-  public void reserve(int pWaggonID, int pPlatzNr, String pKundenname) {
+
+  /**
+   * Reserves a specific seat in a specific wagon for a customer.
+   * @param pWaggonID The identifier of the wagon
+   * @param pSeatNumber The number of the seat to reserve
+   * @param pCustomerName The name of the customer
+   */
+  public void reserve(int pWaggonID, int pSeatNumber, String pCustomerName) {
     searchForWagon(pWaggonID);
-    wagonList.getContent().reserve(pPlatzNr, pKundenname);
+    wagonList.getContent().reserve(pSeatNumber, pCustomerName);
   }
-  public void deleteReservation(int pWaggonID, int pPlatzNr) {
+
+  /**
+   * Deletes a reservation on a specific seat in a specific wagon.
+   * @param pWaggonID The identifier of the wagon
+   * @param pSeatNumber The number of the seat to delete the reservation from
+   */
+  public void deleteReservation(int pWaggonID, int pSeatNumber) {
     searchForWagon(pWaggonID);
-    wagonList.getContent().deleteReservation(pPlatzNr);
+    wagonList.getContent().deleteReservation(pSeatNumber);
   }
-  public int determineFirstFreeSeat(Wagon pWaggon) {
+
+  /**
+   * Determines the first free seat in a given wagon.
+   * @param pWagon The wagon to search for a free seat
+   * @return The number of the first free seat, or -1 if no free seat is found
+   */
+  public int determineFirstFreeSeat(Wagon pWagon) {
+    for (int i = 0; i < pWagon.getNumberOfSeats(); i++) {
+        if (pWagon.isSeatFree(i)) {
+            return i;
+        }
+    }
     return -1;
   }
+
+  /**
+   * Determines the first group of four free seats in the train.
+   * @return An array of two integers representing the number of the wagon and the starting seat number of the free area, or {-1, -1} if no such area is found
+   */
   public int[] determineFirstFourFreeSeats(){
     boolean foundFirstFourSeatGroup = false;
     for(wagonList.toFirst(); wagonList.hasAccess() && !foundFirstFourSeatGroup; wagonList.next()){
@@ -45,6 +101,11 @@ public class Train {
     }
     return new int[]{-1, -1};
   }
+
+  /**
+   * Searches for a wagon in the train.
+   * @param pWagonID The identifier of the wagon to search for
+   */
   public void searchForWagon(int pWagonID){
     for (wagonList.toFirst(); wagonList.getContent().getNumber() != pWagonID && wagonList.hasAccess(); wagonList.next()) {
     }
--- src/main/java/de/tubyoub/seatReservationSystem/service/TrainManagement.java
+++ src/main/java/de/tubyoub/seatReservationSystem/service/TrainManagement.java
@@ -3,37 +3,84 @@ package de.tubyoub.seatReservationSystem.service;
 import de.tubyoub.seatReservationSystem.model.Train;
 import de.tubyoub.seatReservationSystem.util.List;
 
+/**
+ * This class is responsible for managing trains and their reservations.
+ */
 public class TrainManagement {
+  // List of all trains
   List<Train> trains;
+
+  /**
+   * Constructor for TrainManagement class.
+   * Initializes the list of trains.
+   */
   public TrainManagement() {
     trains = new List<Train>();
   }
 
+  /**
+   * Returns the list of all trains.
+   * @return List of trains
+   */
   public List<Train> getTrains() {
     return trains;
   }
 
+  /**
+   * Reserves a seat on a specific train.
+   * @param pTrainID ID of the train
+   * @param pWagonID ID of the wagon
+   * @param pSeatID ID of the seat
+   * @param pCustomer Name of the customer
+   */
   public void reserve(String pTrainID, int pWagonID, int pSeatID, String pCustomer) {
     searchForTrain(pTrainID);
     trains.getContent().reserve(pWagonID, pSeatID, pCustomer);
   }
 
+  /**
+   * Deletes a reservation on a specific train.
+   * @param pTrainID ID of the train
+   * @param pWagonID ID of the wagon
+   * @param pSeatID ID of the seat
+   */
   public void deleteReservation(String pTrainID, int pWagonID, int pSeatID) {
     searchForTrain(pTrainID);
     trains.getContent().deleteReservation(pWagonID, pSeatID);
   }
 
+  /**
+   * Adds a new train to the list.
+   * @param pTrain Train object to be added
+   */
   public void addTrain(Train pTrain) {
     trains.append(pTrain);
   }
+
+  /**
+   * Deletes a train from the list.
+   * @param pTrainID ID of the train to be deleted
+   */
   public void deleteTrain(String pTrainID){
     searchForTrain(pTrainID);
     trains.remove();
   }
+
+  /**
+   * Appends a new wagon to a specific train.
+   * @param pTrainID ID of the train
+   * @param pWagonID ID of the new wagon
+   * @param pSeatQuantity Number of seats in the new wagon
+   */
   public void appendWagon(String pTrainID, int pWagonID, int pSeatQuantity){
     searchForTrain(pTrainID);
     trains.getContent().appendNewWagon(pWagonID, pSeatQuantity);
   }
+
+  /**
+   * Searches for a train in the list.
+   * @param pTrainID ID of the train to be searched
+   */
   public void searchForTrain(String pTrainID){
     for (trains.toFirst(); !trains.getContent().getIdentifier().equals(pTrainID) && trains.hasAccess(); trains.next());
   }

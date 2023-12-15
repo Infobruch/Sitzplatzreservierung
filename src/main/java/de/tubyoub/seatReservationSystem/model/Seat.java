3aaae01d01d824f8903faa303b6fe72cafc77ece
Jörn <github@tubyoub.de>
2023-12-15 13:09:40 +0100

    Added Documentation

--- src/main/java/de/tubyoub/seatReservationSystem/model/Seat.java
+++ src/main/java/de/tubyoub/seatReservationSystem/model/Seat.java
@@ -1,23 +1,46 @@
 package de.tubyoub.seatReservationSystem.model;
+/**
+ * The Seat class represents a seat in the seat reservation system.
+ * It contains a boolean indicating whether the seat is reserved and a string representing the name of the customer who reserved the seat.
+ */
 
 public class Seat {
     private boolean reserved;
 
     private String reservedFor;
 
+    /**
+     * Constructor for the Seat class.
+     * Initializes the seat as not reserved and the reservation as null.
+     */
     public Seat() {
         reserved = false;
         reservedFor = null;
     }
 
+    /**
+     * Checks if the seat is reserved.
+     *
+     * @return True if the seat is reserved, false otherwise.
+     */
     public boolean isReserved() {
         return reserved;
     }
 
+    /**
+     * Returns the name of the customer who reserved the seat.
+     *
+     * @return The name of the customer who reserved the seat.
+     */
     public String getReservation() {
         return reservedFor;
     }
 
+    /**
+     * Reserves the seat for a specific customer.
+     *
+     * @param pKundenname The name of the customer.
+     */
     public void setReservation(String pCustomerName) {
         if (!reserved) {
             reserved = true;
@@ -25,6 +48,9 @@ public class Seat {
         }
     }
 
+    /**
+     * Deletes the reservation for the seat.
+     */
     public void delReservation() {
         if (reserved) {
             reserved = false;
--- src/main/java/de/tubyoub/seatReservationSystem/service/TrainManagement.java
+++ src/main/java/de/tubyoub/seatReservationSystem/service/TrainManagement.java
@@ -3,21 +3,53 @@ package de.tubyoub.seatReservationSystem.service;
 import de.tubyoub.seatReservationSystem.model.Train;
 import de.tubyoub.seatReservationSystem.util.List;
 
+/**
+ * The TrainManagement class provides methods for managing trains in the seat reservation system.
+ * It contains methods for getting trains, reserving seats, deleting reservations, and adding trains.
+ */
 public class TrainManagement {
 
+  /**
+ * The TrainManagement class provides methods for managing trains in the seat reservation system.
+ * It contains methods for getting trains, reserving seats, deleting reservations, and adding trains.
+ */
   public TrainManagement() {
   }
 
+  /**
+ * The TrainManagement class provides methods for managing trains in the seat reservation system.
+ * It contains methods for getting trains, reserving seats, deleting reservations, and adding trains.
+ */
   public List<Train> gibZuege() {
     return null;
   }
 
+  /**
+     * Reserves a seat in a specific wagon of a specific train for a customer.
+     *
+     * @param pZugBezeichner The identifier of the train.
+     * @param pWaggonNr The number of the wagon.
+     * @param pPlatzNr The number of the seat.
+     * @param pKundenname The name of the customer.
+     */
   public void reserve(String pZugBezeichner, int pWaggonNr, int pPlatzNr, String pKundenname) {
   }
 
+  /**
+     * Deletes a reservation for a seat in a specific wagon of a specific train.
+     *
+     * @param pZugBezeichner The identifier of the train.
+     * @param pWaggonNr The number of the wagon.
+     * @param pPlatzNr The number of the seat.
+     */
   public void deleteReservation(String pZugBezeichner, int pWaggonNr, int pPlatzNr) {
   }
 
+  /**
+     * Adds a new train to the system.
+     *
+     * @param pTrain The train to add.
+     */
   public void addTrain(Train pTrain) {
 
   }
